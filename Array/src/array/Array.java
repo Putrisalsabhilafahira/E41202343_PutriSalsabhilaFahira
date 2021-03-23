ee60b6bb3a5e4b16ede39b6ada36a5cbc02f6912
Putrisalsabhilafahira <80686459+Putrisalsabhilafahira@users.noreply.github.com>
2021-03-23 21:37:27 +0700

    Add files via upload

---
+++ Array/src/array/Array.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author Putri Salsabhila Fahira
+ */
+public class Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] teks = {"Putri","Salsabhila","Fahira"};
+        
+        for(int j=0; j<teks.length; j++) {
+            System.out.print(teks[j]);
+            System.out.print("");
+        }
+    }
+    
+}
